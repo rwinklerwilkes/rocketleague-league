414c8ee990bb52df088c5e9c4c95a5ddee6bee2d
Rich Winkler <rich.w.sas@gmail.com>
2016-01-17 17:05:07 -0500

    more action hooks

--- wsgi/rocketleague/settings.py
+++ wsgi/rocketleague/settings.py
@@ -119,4 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT =path.join(PROJECT_ROOT,'static')
+STATIC_URL =path.join(PROJECT_ROOT,'static')
