0779fa86425d0361ab38b9d681ed0436c4baa252
Rich Winkler <rich.w.sas@gmail.com>
2016-01-18 19:31:08 -0500

    signals, css

--- wsgi/stats/apps.py
+++ wsgi/stats/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class StatsConfig(AppConfig):
     name = 'stats'
+    verbose_name = 'Rocket League Stats'
+
+    def ready(self):
+        import stats.signals
--- wsgi/stats/tests.py
+++ wsgi/stats/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import GameWeek,GameStats,Game,Season,Team,Player
 
 # Create your tests here.
+class SignalTests(TestCase):
+    def test_gamestats_add_signal(self):
+        test_team = Team(key='C',name='test')
+        test_team.save()
+        test_season = Season(slug='2016',name='test')
+        test_season.save()
+        test_gameweek = GameWeek(season = test_season,number=1,starts_at = timezone.now(), ends_at = timezone.now())
+        test_gameweek.save()
+        test_game = Game(gameweek=test_gameweek,outcome='C',home_team = test_team,home_team_score=300,away_team_score=300,away_team=test_team)
+        test_game.save()
+        test_player = Player(player_first_name='Test',player_last_name='Tester',player_nickname='Testes',player_team=test_team)
+        test_player.save()
+        test_gamestats = GameStats(game=test_game,player=test_player,goals=3,assists=3,saves=3,shots=3,points=300)
+        test_gamestats.save()
+        self.assertEqual(test_player.lifetime_points,test_gamestats.points)
