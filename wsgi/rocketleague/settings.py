7623535a95fb2b2ab86b8aa6b1fce29df6625380
Rich Winkler <rich.w.sas@gmail.com>
2016-01-21 18:48:17 -0500

    fixing static files

--- wsgi/rocketleague/settings.py
+++ wsgi/rocketleague/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATICFILES_DIRS = [
-	os.path.join(WSGI_DIR, 'static'),
 	os.path.join(WSGI_DIR, 'static/stats/'),
 	os.path.join(WSGI_DIR, 'stats/static/')
 ]
