1186aaa0f464a543233f3a45500c02ef1c29b293
Rich Winkler <rich.w.sas@gmail.com>
2016-01-22 17:49:24 -0500

    urls, redirects

--- wsgi/schedule/views.py
+++ wsgi/schedule/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login
 
 def login(request):
-    username = request.POST['username']
-    password = request.POST['password']
+    username = request.POST.get('username')
+    password = request.POST.get('password')
     
     user = authenticate(username=username,password=password)
 
