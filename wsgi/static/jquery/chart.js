f5b311b2a15857b804ce662b681d275548508443
Rich Winkler <rich.w.sas@gmail.com>
2016-01-29 22:49:09 -0500

    team table

---
+++ wsgi/static/jquery/chart.js
@@ -0,0 +1,100 @@
+/* Filename: base.js */
+//$(document).ready(function() {
+//	$('ul.nav a').hover(function() {
+//		$('#triangle-up').css('border-bottom','30px solid #bcd2ee');
+//	}, function() {
+//	//on mouseout, reset the color
+//	$('#triangle-up').css('border-bottom','30px solid #3D3D3D');
+//	});
+//});
+
+  $(document).ready(function () { 
+    var newHeight = $(window).height() - $('#header-wrapper').height() - $('#menu-wrapper').height() - $('#copyright').height();
+    var oldHeight = $('#featured-wrapper').height();
+
+    if (newHeight > oldHeight) {
+      $('#featured-wrapper').height(newHeight);
+    }
+  });
+
+
+google.load('visualization','1.0',{'packages':['corechart']});
+
+$(document).ready(function () {
+	chartData('All','All','goals');
+});
+
+function chartData(season,week,out) {
+	$.ajax(
+	{
+	type:'GET',
+	url:'/schedule/chart_data/',
+	data:{'season':season,'week':week,'out':out},
+	success:function(dataFromServer) {drawChart(dataFromServer);}
+	}
+	);
+}
+
+//creates and populates data table, instantiates chart, passes in data, and draws
+function drawChart(dataFromServer) {
+	var data = new google.visualization.DataTable();
+	
+	var stats = dataFromServer['stats'];
+	console.log(stats);
+
+	var col1 = stats[0][0];
+	console.log(col1)
+	var col2 = stats[0][1];
+	console.log(col2)
+	data.addColumn('string',col1);
+	data.addColumn('number',col2);
+	
+	for (i=1;i<stats.length;i++) {
+		data.addRow(stats[i]);
+	}
+
+	var options = {
+	width: $(window).width()/3,
+	height: 400,
+	hAxis: {
+		title: 'Game'
+	},
+	vAxis: {
+		title: 'Points Scored'
+	}
+	};
+
+	var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+	chart.draw(data,options);
+}
+
+
+$(function() {
+	$(".dropdown-menu").on('click','li a', function () {
+		var btn = $('.btn#' + $(this).parents('ul').attr('id'));
+		btn.html($(this).text() + ' <span class="caret"></span>');
+		btn.val($(this).text());
+		
+		var other = '';
+		//call function to redraw graph
+		if ($(this).parents('ul').attr('id')=='week-dropdown') {
+			other = 'season-dropdown';
+		}
+		else {
+			other = 'week-dropdown';
+		}
+		var otBtn = $('.btn#' + other);
+		
+		console.log(otBtn.val());
+		console.log(btn.val());
+		
+		if (other == 'season-dropdown') {
+			chartData(otBtn.val(),btn.val(),'goals');
+		}
+		else {
+			chartData(btn.val(),otBtn.val(),'goals');
+		}
+	});
+});
+
--- wsgi/stats/views.py
+++ wsgi/stats/views.py
@@ -17,7 +17,60 @@ def players(request):
 def season(request,season_slug):
     season = get_object_or_404(Season,slug=season_slug)
     weeks = season.gameweek_set.all()
-    return render(request,'stats/season.html',{'season':season,'weeks':weeks})
+
+    ret = {}
+
+    #wins, losses, games for, games against, goals for, goals against
+    for w in weeks:
+        games = w.game_set.all()
+        week_res = {}
+        
+        for g in games:
+            if g.home_team.key not in ret.keys():
+                ret[g.home_team.key] = {'name':g.home_team.name,'wins':0,'losses':0,'for':0,'against':0,'goals_for':0,'goals_against':0}
+            if g.away_team.key not in ret.keys():
+                ret[g.away_team.key] = {'name':g.away_team.name,'wins':0,'losses':0,'for':0,'against':0,'goals_for':0,'goals_against':0}
+            if g.home_team.key not in week_res.keys():
+                week_res[g.home_team.key] = 0
+            if g.away_team.key not in week_res.keys():
+                week_res[g.away_team.key] = 0
+            winner = ''
+            loser = ''
+            if g.outcome == g.home_team.key:
+                week_res[g.home_team.key] += 1
+                winner = 'ht'
+                loser = 'at'
+            else:
+                week_res[g.away_team.key] += 1
+                winner = 'at'
+                loser = 'ht'
+            if winner == 'ht':
+                ret[g.home_team.key]['for']+=1
+                ret[g.home_team.key]['goals_for']+=g.home_team_score
+                ret[g.away_team.key]['against']+=1
+                ret[g.away_team.key]['goals_against']+=g.away_team_score
+            else:
+                ret[g.home_team.key]['against']+=1
+                ret[g.home_team.key]['goals_against']+=g.home_team_score
+                ret[g.away_team.key]['for']+=1
+                ret[g.away_team.key]['goals_for']+=g.away_team_score
+            if week_res[g.away_team.key] + week_res[g.home_team.key] == 3:
+                if week_res[g.home_team.key] > week_res[g.away_team.key]:
+                    ret[g.home_team.key]['wins']+=1
+                    ret[g.away_team.key]['losses']+=1
+                else:
+                    ret[g.home_team.key]['losses']+=1
+                    ret[g.away_team.key]['wins']+=1
+
+
+    output = []
+
+    for k in ret.keys():
+        outlist = [ret[k]['name'],ret[k]['wins'],ret[k]['losses'],ret[k]['for'],ret[k]['against'],ret[k]['goals_for'],ret[k]['goals_against']]
+        output.append(outlist)
+    output = sorted(output,key=lambda l:l[1],reverse=True)
+
+    return render(request,'stats/season.html',{'season':season,'weeks':weeks,'games':games,'table':output})
 
 def week(request,season_slug,week_number):
     season = get_object_or_404(Season,slug=season_slug)
